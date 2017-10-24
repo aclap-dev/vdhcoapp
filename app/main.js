bc420244bdab81ed1d408c33fb5bde4a88432df5
mig <michel.gutierrez@gmail.com>
2017-10-24 23:55:21 +0200

    added source code GPL headers

--- app/main.js
+++ app/main.js
@@ -1,14 +1,23 @@
 /*
- * weh - WebExtensions Helper
- *
- * @summary workflow and base code for developing WebExtensions browser add-ons
- * @author Michel Gutierrez
- * @link https://github.com/mi-g/weh
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+vdhcoapp: Video DownloadHelper Companion app
+
+Copyright (C) 2017  downloadhelper.net
+
+This file is part of vdhcoapp.
+
+Vdhcoapp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Vdhcoapp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Vdhcoapp. If not, see <http://www.gnu.org/licenses/>
+*/
 
 if(process.argv[2]=="install")
 	require("./native-autoinstall").install();
