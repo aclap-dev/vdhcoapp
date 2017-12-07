cefbe9ba6dfb801fcbab2e3f4466fd8c870c0774
mig <michel.gutierrez@gmail.com>
2017-12-07 18:14:48 +0100

    added support for binary requests

--- app/request.js
+++ app/request.js
@@ -36,17 +36,66 @@ function GetData(id) {
 		throw new Error("No such request id");
 	if(reqInfo.timer)
 		clearTimeout(reqInfo.timer);
-	var data = reqInfo.data.substr(reqInfo.position,Math.min(MAX_SIZE,reqInfo.data.length-reqInfo.position));
-	reqInfo.position += data.length;
-	var more = true;
-	if(reqInfo.position==reqInfo.data.length) {
-		more = false;
+	if(reqInfo.error) {
 		delete requestStore[id];
-	} else {
-		reqInfo.timer = setTimeout(()=>{
-			logger.warn("Expired data for request",reqInfo.url);
+		throw reqInfo.error;
+	}
+	reqInfo.timer = setTimeout(()=>{
+		logger.warn("Expired data for request",reqInfo.url);
+		if(reqInfo.reject) {
+			reqInfo.reject(new Error("Timeout"));
+			delete reqInfo.resolve;
+			delete reqInfo.reject;
+		}
+		delete requestStore[id];
+	},EXPIRE_DATA_TIMEOUT);
+	var data, more = true;
+	if(reqInfo.type=="buffer") {
+		var retBuffers = [];
+		var retLength = 0;
+		while(reqInfo.data.length>0 && retLength+reqInfo.data[0].length<MAX_SIZE) {
+			let buffer = reqInfo.data.shift();
+			retBuffers.push(buffer);
+			retLength += buffer.length;
+		}
+		var remainingLength = MAX_SIZE-retLength;
+		if(reqInfo.data.length>0 && remainingLength>0) {
+			let buffer = reqInfo.data.shift();
+			let buffer2 = buffer.slice(0,remainingLength);
+			retBuffers.push(buffer2);
+			retLength += buffer2.length;
+			let buffer3 = buffer.slice(remainingLength);
+			reqInfo.data.unshift(buffer3);
+		}
+		more = reqInfo.running || reqInfo.data.length>0;
+		if(!more) {
 			delete requestStore[id];
-		},EXPIRE_DATA_TIMEOUT);
+			clearTimeout(reqInfo.timer);
+			reqInfo.timer = null;
+		}
+		if(retBuffers.length>0) {
+			data = new Buffer(retLength);
+			let length = 0;
+			retBuffers.forEach((buf)=>{
+				data.set(buf,length);
+				length+=buf.length;
+			});
+		} else if(!more)
+			data = new Buffer(0);
+		else
+			return new Promise((resolve, reject) => {
+				reqInfo.resolve = resolve;
+				reqInfo.reject = reject;				
+			});
+	} else {
+		data = reqInfo.data.substr(reqInfo.position,Math.min(MAX_SIZE,reqInfo.data.length-reqInfo.position));
+		reqInfo.position += data.length;
+		if(reqInfo.position==reqInfo.data.length) {
+			more = false;
+			delete requestStore[id];
+			clearTimeout(reqInfo.timer);
+			reqInfo.timer = null;
+		}
 	}
 	return {
 		id,
@@ -55,17 +104,21 @@ function GetData(id) {
 	}
 }
 
+function GotHeaders(headers=[]) {
+	var gotHeaders = {};
+	headers.forEach((header)=>{
+		if(typeof header.value !== "undefined")
+			gotHeaders[header.name] = header.value;
+		else
+			gotHeaders[header.name] = Buffer.from(header.binaryValue);
+	});
+	return gotHeaders;
+}
+
 rpc.listen({
-	"request": (url,options={}) => {
-		var gotHeaders = {};
-		(options.headers||[]).forEach((header)=>{
-			if(typeof header.value !== "undefined")
-				gotHeaders[header.name] = header.value;
-			else
-				gotHeaders[header.name] = Buffer.from(header.binaryValue);
-		});
-		options.headers = gotHeaders;
-	
+	"request": (url,options) => {
+		options = options || {};
+		options.headers = GotHeaders(options.headers);	
 		return new Promise((resolve, reject) => {
 			var id = ++currentIndex;
 			got.get(url,options)
@@ -83,5 +136,45 @@ rpc.listen({
 	},
 	"requestExtra": (id) => {
 		return GetData(id);
+	},
+	"requestBinary": (url,options) => {
+		options = options || {};
+		var id = ++currentIndex;
+		var reqInfo = requestStore[id];
+		if(!reqInfo)
+			reqInfo = requestStore[id] = {
+				id,
+				type: "buffer",
+				data: [],
+				running: true
+			}
+		options.headers = GotHeaders(options.headers);
+		got.stream(url,options)
+			.on('error',(err)=>{
+				if(reqInfo.timer)
+					clearTimeout(reqInfo.timer);
+				if(reqInfo.reject) {
+					delete requestStore[id];
+					reqInfo.reject(err);
+				} else
+					reqInfo.error = err;
+			})
+			.on('data',(data)=>{
+				reqInfo.data.push(data);
+				if(reqInfo.resolve) {
+					reqInfo.resolve(GetData(id));
+					delete reqInfo.resolve;
+					delete reqInfo.reject;
+				}
+			})
+			.on('end',()=>{
+				reqInfo.running = false;
+				if(reqInfo.resolve) {
+					reqInfo.resolve(GetData(id));
+					delete reqInfo.resolve;
+					delete reqInfo.reject;	
+				}
+			});
+			return GetData(id);
 	}
 });
