1b125f3e4cfd4ccb6d0cad5d75d18eabcdbdc539
Paul Rouget <me@paulrouget.com>
2023-08-21 14:04:42 +0800

    assets/mac

--- scripts/mac.sh
+++ scripts/mac.sh
@@ -32,20 +32,20 @@ cp $dist/app.bin $macos_dir/$binary_name
 echo "Creating pkg config files"
 
 cp LICENSE.txt assets/README.txt $macos_dir/
-cp assets/icon.icns $res_dir
+cp assets/mac/icon.icns $res_dir
 
 tmpfile=$(mktemp /tmp/coapp-build.XXXXXX)
 jq -n --argfile config config.json \
   --argfile package package.json \
   "{config:\$config, manifest:\$package, version:$pkg_version, binaryName:\"$binary_name\"}" \
   > $tmpfile
-ejs -f $tmpfile ./assets/pkg-distribution.xml.ejs \
+ejs -f $tmpfile ./assets/mac/pkg-distribution.xml.ejs \
   > $dist/pkg-distribution.xml
-ejs -f $tmpfile ./assets/pkg-component.plist.ejs \
+ejs -f $tmpfile ./assets/mac/pkg-component.plist.ejs \
   > $dist/pkg-component.plist
-ejs -f $tmpfile ./assets/Info.plist.ejs \
+ejs -f $tmpfile ./assets/mac/Info.plist.ejs \
   > $app_path/Contents/Info.plist
-ejs -f $tmpfile ./assets/setup-mac-pkg.sh.ejs \
+ejs -f $tmpfile ./assets/mac/postinstall.ejs \
   > $scripts_dir/postinstall
 rm $tmpfile
 
@@ -77,7 +77,7 @@ echo "Building DMG file"
 
 # FIXME: doc about create-dmg
 create-dmg --volname "DownloadHelper Co-app" \
-  --background ./assets/dmg-background.tiff \
+  --background ./assets/mac/dmg-background.tiff \
   --window-pos 200 120 --window-size 500 400 --icon-size 70 \
   --icon "net.downloadhelper.coapp.app" 100 200 \
   --app-drop-link 350 200 \
