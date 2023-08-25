43330971ecfcd49d08e62816ee4fc53fb5112ff5
Paul Rouget <me@paulrouget.com>
2023-08-25 09:07:43 +0000

    linux pkg

--- scripts/create_package_linux.sh
+++ scripts/create_package_linux.sh
@@ -7,15 +7,18 @@ app_version=$(jq -r '.version' ./package.json)
 echo "Extracting ffmpeg"
 
 cd $dist
+mkdir bin
+mv $dist/app.bin $dist/bin/$binary_name
 tar -xf $ffmpeg_tarball
-mv ffmpeg-linux-$target_arch/ffmpeg .
-mv ffmpeg-linux-$target_arch/ffprobe .
+mv ffmpeg-linux-$target_arch/ffmpeg bin
+mv ffmpeg-linux-$target_arch/ffprobe bin
 mv ffmpeg-linux-$target_arch/presets ffmpeg-presets
 rmdir ffmpeg-linux-$target_arch
 cd -
 
-mv $dist/app.bin $dist/$binary_name
 
-cp node_modules/open/xdg-open $dist
+jq "{id, name, description, allowed_extensions}" ./config.json > $dist/config.json
+
+cp node_modules/open/xdg-open $dist/bin
 
 cp LICENSE.txt assets/README.txt $dist
