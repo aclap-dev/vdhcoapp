b90b4e2dd6d7c0c6f4393984a79f7770ad3a79c0
Paul Rouget <me@paulrouget.com>
2023-08-25 16:44:01 +0800

    dynamic resdir

--- app/pkgfiles.js
+++ app/pkgfiles.js
@@ -2,7 +2,14 @@
 
 const path = require('path');
 const exec_dir = path.dirname(process.execPath);
-const resource_dir = path.join(exec_dir, "../Resources/");
+
+let resource_dir;
+
+if (process.platform === "darwin") {
+  resource_dir = path.join(exec_dir, "../Resources/");
+} else {
+  resource_dir = path.join(exec_dir, "./resources/");
+}
 
 exports.ffmpeg = path.join(exec_dir, "ffmpeg");
 exports.ffprobe = path.join(exec_dir, "ffprobe");
