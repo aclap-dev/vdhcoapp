859642106f45df926e0fa0af32906e58c3e0867c
Paul Rouget <me@paulrouget.com>
2023-09-03 12:28:32 +0800

    flatpack WIP

--- app/src/native-autoinstall.js
+++ app/src/native-autoinstall.js
@@ -1,8 +1,10 @@
 const os = require("os");
-const fs = require("fs-extra");
+const fs = require("fs");
 const path = require("path");
-const { spawn } = require('child_process');
+const { spawn, spawnSync } = require('child_process');
 const config = require('../../dist/config.json');
+
+const STORES = ["mozilla", "google", "microsoft"];
 
 function DisplayMessage(body, title) {
   if (title) {
@@ -35,6 +37,8 @@ function GetMode() {
   let mode;
   if (process.argv.indexOf("--user") >= 0) {
     mode = "user";
+  } else if (process.argv.indexOf("--flatpack") >= 0) {
+    mode = "platform";
   } else if (process.argv.indexOf("--system") >= 0) {
     mode = "system";
   } else if (!process.execPath.startsWith(os.homedir())) {
@@ -42,13 +46,60 @@ function GetMode() {
   } else {
     mode = "user";
   }
+
+  if (mode == "system") {
+    if (!process.getuid() == 0) {
+      console.error("Can't install system wide without root privilege. Re-run with sudo.");
+      process.exit(1);
+    }
+  }
+
+  if (mode == "flatpack") {
+    let process = spawnSync("flatpack", ["-v"]);
+    if (process.error) {
+      console.error("flatpack returned an error", process.error);
+      process.exit(1);
+    }
+  }
   return mode;
+}
+
+async function InstallBinariesInFlatpack(uninstall) {
+  let dir = path.dirname(process.execPath);
+  let binaries = ["ffmpeg", "ffprobe", config.package.binary_name].map((name) => {
+    return path.resolve(dir, name);
+  });
+  for (let store of STORES) {
+    let packs = config.store[store].msg_manifest_paths.flatpack;
+    for (let pack of packs) {
+      dir = path.resolve(os.homedir(), ".var/app", pack);
+      if (fs.existsSync(dir)) {
+        console.log(`Flatpack for ${pack} seems to exist. Installing.`);
+        dir = path.resolve(dir, config.meta.id);
+        if (uninstall && fs.existsSync(dir)) {
+          fs.unlink(dir, {recursive: true});
+        } else {
+          let xxx = fs.mkdir(dir, {recursive: true});
+          console.log("FIXME", xxx);
+          for (let bin of binaries) {
+            fs.copyFileSync(bin, dir);
+          }
+          let args = `run --command=${binaries[2]} ${pack} install --user`;
+          let process = spawnSync("flatpack", args.split(" "));
+          if (process.error) {
+            console.error("flatpack returned an error", process.error);
+            process.exit(1);
+          }
+        }
+      }
+    }
+  }
 }
 
 function SetupFiles(platform, mode, uninstall) {
   let manifests = BuildManifests();
   let ops = [];
-  ["mozilla", "google", "microsoft"].forEach((store) => {
+  for (let store of STORES) {
     let directories = config.store[store].msg_manifest_paths[platform][mode];
     directories.forEach((dir) => {
       if (mode == "user") {
@@ -59,7 +110,7 @@ function SetupFiles(platform, mode, uninstall) {
         content: JSON.stringify(manifests[store], " ", 2)
       });
     });
-  });
+  }
 
   for (let op of ops) {
     if (uninstall) {
@@ -87,7 +138,6 @@ function SetupFiles(platform, mode, uninstall) {
     text = config.meta.name + " is ready to be used";
   }
   DisplayMessage(text, config.meta.name);
-  process.exit(0);
 }
 
 function install_uninstall(uninstall = false) {
@@ -95,6 +145,8 @@ function install_uninstall(uninstall = false) {
   let platform = os.platform();
   if (platform == "darwin") {
     SetupFiles("mac", mode, uninstall);
+  } else if (platform == "linux" && mode == "flatpack") {
+    InstallBinariesInFlatpack(uninstall);
   } else if (platform == "linux") {
     SetupFiles("linux", mode, uninstall);
   } else {
